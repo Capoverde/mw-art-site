0716bd9ee538ba0ae6592580c3e3b50b22a9e3d8
Piotr <piotr_as@yahoo.com>
2021-11-02 10:38:09 +0100

    added hamburger

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import '../scss/app.scss'
 /* Demo JS */
 import './demo.js'
+import './hamburger.js'
 
 /* Your JS Code goes here */
 console.log('%c Hello from app.js', 'color: #ffc600; background: #000; padding: 5px 10px; margin: 5px 25px; font-size:2em;')
